dc2048df244c4fe08bc7c0ba54b047535e739430
liemhoanglong <43882959+liemhoanglong@users.noreply.github.com>
2021-09-14 23:52:49 +0700

    edit color type objectId in cart of user

--- routes/cates.routes.js
+++ routes/cates.routes.js
@@ -11,8 +11,8 @@ router.get('/by-list-and-group/:catelist/:categroup', cateController.getByCategr
 router.get('/by-list/:id', cateController.getByCatelist);
 router.get('/:id', cateController.getOne);
 router.post('/', userAuth, adminAuth, cateController.create); //need login with admin
-router.put('/:id', cateController.update); //need login with admin
-// router.put('/:id', userAuth, adminAuth, cateController.update); //need login with admin
+// router.put('/:id', cateController.update); //need login with admin
+router.put('/:id', userAuth, adminAuth, cateController.update); //need login with admin
 router.delete('/:id', userAuth, adminAuth, cateController.delete); //need login with admin
 
 module.exports = router;
