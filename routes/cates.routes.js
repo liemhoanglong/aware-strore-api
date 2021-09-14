f68b08944c9d476e6cdf32f5b0964695faf9f169
liemhoanglong <43882959+liemhoanglong@users.noreply.github.com>
2021-09-14 11:53:58 +0700

    fix api find cate by catelist and find cate by catelist and group

--- routes/cates.routes.js
+++ routes/cates.routes.js
@@ -7,9 +7,12 @@ const router = express.Router();
 /* GET users listing. */
 router.get('/', cateController.getAll);
 router.get('/by-group/:id', cateController.getByCateGroup);
+router.get('/by-list-and-group/:catelist/:categroup', cateController.getByCategroupAndCatelist);
+router.get('/by-list/:id', cateController.getByCatelist);
 router.get('/:id', cateController.getOne);
 router.post('/', userAuth, adminAuth, cateController.create); //need login with admin
-router.put('/:id', userAuth, adminAuth, cateController.update); //need login with admin
+router.put('/:id', cateController.update); //need login with admin
+// router.put('/:id', userAuth, adminAuth, cateController.update); //need login with admin
 router.delete('/:id', userAuth, adminAuth, cateController.delete); //need login with admin
 
 module.exports = router;
