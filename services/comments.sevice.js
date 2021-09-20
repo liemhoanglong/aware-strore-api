ce063f5d2b30bfa5bfdc11e5d38b3e6616dac6fc
liemhoanglong <43882959+liemhoanglong@users.noreply.github.com>
2021-09-20 17:33:50 +0700

    fix api comment

--- services/comments.sevice.js
+++ services/comments.sevice.js
@@ -1,3 +1,4 @@
+const commentsController = require('../controllers/comments.controller');
 const commentModel = require('../model/comment.model');
 
 module.exports = {
@@ -5,18 +6,33 @@ module.exports = {
 		let res = commentModel.find();
 		return res
 	},
+	getOneByOrder: (authorId, query) => {
+		console.log(query)
+		return res = commentModel.findOne({
+			authorId,
+			orderId: query.orderId,
+			productId: query.productId,
+			color: query.color,
+			size: query.size,
+		});
+	},
 	getOne: (id) => {
 		return res = commentModel.findById(id);
 	},
-	create: async (name) => {
+	create: async (authorId, data) => {
+		const { title, content, star, productId, color, size, orderId } = data;
 		const temp = new commentModel({
-			name
+			title, content, star, authorId, productId, color, size, orderId
 		});
 		return await temp.save();
 	},
-	update: async (id, name) => {
+	update: async (id, authorId, data) => {
+		const { title, content, star } = data;
 		let temp = await commentModel.findById(id);
-		temp.name = name;
+		if (JSON.stringify(authorId) != JSON.stringify(temp.authorId)) return;
+		temp.title = title;
+		temp.content = content;
+		temp.star = star;
 		return await temp.save();
 	},
 	delete: (id) => {//need to check cate group belong
